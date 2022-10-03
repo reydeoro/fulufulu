51c34201762f5de80d7a7ceee031b6abd1878346
reydeoro <senaotantanmen@gmail.com>
2022-10-04 01:32:03 +0900

    テキストを変更してみる

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'ハローワールド',
+              'テストできればそれでいい',
             ),
             Text(
               '$_counter',
