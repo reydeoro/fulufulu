c28ada9f4989db27b96540a8e7e2dd75e8a1f055
reydeoro <senaotantanmen@gmail.com>
2022-10-04 01:35:21 +0900

    dartファイルを動かすテスト

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'テストできればそれでいい',
+              '試験用パッケージ',
             ),
             Text(
               '$_counter',
